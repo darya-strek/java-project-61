06c75aa9795102b5107029d2eb406afcbab85abc
darya-strekalovskaya <darya.strek@gmail.com>
2023-03-26 23:04:48 +0300

    Fix GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -3,31 +3,35 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class GCD {
-    public static void startGameGCD() {
 
-        int stepOfGame = Engine.FIRST_STEP_OF_GAME;
-        int maxStepOfGame = Engine.MAX_STEP_OF_GAME;
+    private static final int MIN_RANDOM_NUMBER = 1;
+    private static final int MAX_RANDOM_NUMBER = 100;
 
-        String descriptionOfGame = "Find the greatest common divisor of given numbers.";
-
-        String[] questions = new String[maxStepOfGame];
-        String[] rightAnswers = new String[maxStepOfGame];
-
-        for (var i = stepOfGame; i < maxStepOfGame; i += 1) {
-
-            int firstNumber = Engine.getRangomNumber(Engine.MAX_RANDOM_NUMBER, Engine.MIN_RANDOM_NUMBER);
-            int secondNumber = Engine.getRangomNumber(Engine.MAX_RANDOM_NUMBER, Engine.MIN_RANDOM_NUMBER);
-
-            questions[i] = firstNumber + " " + secondNumber;
-
-            int currentRightAnswer = getGreatestCommonDivisor(firstNumber, secondNumber);
-            rightAnswers[i] = Integer.toString(currentRightAnswer);
-        }
-
-        Engine.game(descriptionOfGame, questions, rightAnswers);
-    }
+    private static final String DESCRIPTION = "Find the greatest common divisor of given numbers.";
 
     public static int getGreatestCommonDivisor(int number1, int number2) {
         return (number1 % number2 == 0) ? number2 : getGreatestCommonDivisor(number2, number1 % number2);
     }
+
+    public static void startGameGCD() {
+
+        final int firstStepOfGame = 0;
+        final int maxStepOfGame = 3;
+
+        String[][] questionsAndAnswers = new String[maxStepOfGame][2];
+
+        for (var i = firstStepOfGame; i < maxStepOfGame; i += 1) {
+
+            int firstNumber = Utils.generateRangomNumber(MAX_RANDOM_NUMBER, MIN_RANDOM_NUMBER);
+            int secondNumber = Utils.generateRangomNumber(MAX_RANDOM_NUMBER, MIN_RANDOM_NUMBER);
+
+            questionsAndAnswers[i][0] = firstNumber + " " + secondNumber;
+
+            int currentRightAnswer = getGreatestCommonDivisor(firstNumber, secondNumber);
+            questionsAndAnswers[i][1] = Integer.toString(currentRightAnswer);
+        }
+
+        Engine.game(DESCRIPTION, questionsAndAnswers);
+    }
+
 }
