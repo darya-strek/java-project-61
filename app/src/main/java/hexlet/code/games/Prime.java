adbc633d6e8710bae6a9a49182ea01c4c60e8d92
darya-strekalovskaya <darya.strek@gmail.com>
2023-03-26 23:05:13 +0300

    Fix Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -3,31 +3,39 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Prime {
-    public static void startGamePrime() {
 
-        int stepOfGame = Engine.FIRST_STEP_OF_GAME;
-        int maxStepOfGame = Engine.MAX_STEP_OF_GAME;
+    private static final int MIN_RANDOM_NUMBER = -100;
+    private static final int MAX_RANDOM_NUMBER = 100;
 
-        String descriptionOfGame = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-
-        String[] questions = new String[maxStepOfGame];
-        String[] rightAnswers = new String[maxStepOfGame];
-
-        for (var i = stepOfGame; i < maxStepOfGame; i += 1) {
-            int currentRandomNumber = Engine.getRangomNumber(Engine.MAX_RANDOM_NUMBER, Engine.MIN_RANDOM_NUMBER);
-            questions[i] = Integer.toString(currentRandomNumber);
-            rightAnswers[i] = isPrime(currentRandomNumber) ? "yes" : "no";
-        }
-
-        Engine.game(descriptionOfGame, questions, rightAnswers);
-    }
+    private static final String DESCRIPTION = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 
     public static boolean isPrime(int number) {
-        for (var n = 2; n <= (number / 2); n += 1) {
-            if (number % n == 0) {
-                return false;
+        if (number <= 0) {
+            return false;
+        } else {
+            for (var n = 2; n <= (number / 2); n += 1) {
+                if (number % n == 0) {
+                    return false;
+                }
             }
         }
         return true;
     }
+
+    public static void startGamePrime() {
+
+        final int firstStepOfGame = 0;
+        final int maxStepOfGame = 3;
+
+        String[][] questionsAndAnswers = new String[maxStepOfGame][2];
+
+        for (var i = firstStepOfGame; i < maxStepOfGame; i += 1) {
+            int currentRandomNumber = Utils.generateRangomNumber(MAX_RANDOM_NUMBER, MIN_RANDOM_NUMBER);
+            questionsAndAnswers[i][0] = Integer.toString(currentRandomNumber);
+            questionsAndAnswers[i][1] = isPrime(currentRandomNumber) ? "yes" : "no";
+        }
+
+        Engine.game(DESCRIPTION, questionsAndAnswers);
+    }
+
 }
