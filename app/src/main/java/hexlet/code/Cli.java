a7fbf27e5dd897d41360c3952f1b0d43f053d20d
darya-strekalovskaya <darya.strek@gmail.com>
2023-03-26 23:02:58 +0300

    Fix Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,16 +2,14 @@ package hexlet.code;
 
 import java.util.Scanner;
 public class Cli {
-    private static String userName;
-    public static void greetings() {
+    public static void greet() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nWelcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        userName = scanner.next();
+        String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
-    }
-    public static String getUserName() {
-        return userName;
+
+        scanner.close();
     }
 }
